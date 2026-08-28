284cc2883363bb496a779e1f792748822c771db6
Gabriel_31997 <gabrielwkun@gmail.com>
2026-08-28 20:54:22 +0000

    deploy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import $ from "jquery";
-
-$(document).ready(function () {
-  console.log("jQuery is working inside Vite!");
+// Example: Tracking a WhatsApp icon click
+document.querySelector("#whatsapp-btn")?.addEventListener("click", () => {
+  if (typeof gtag === "function") {
+    gtag("event", "click", {
+      event_category: "Contact",
+      event_label: "WhatsApp Button",
+    });
+  }
 });
